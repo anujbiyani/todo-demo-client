4f996b7415cc24e9541e12fc404851f9d1799a04
Anuj Biyani <anujbiyani01@gmail.com>
2017-06-02 16:36:59 -0700

    make each todo list clickable
    currently does not navigate anywhere

--- src/app/todo-lists/todo-lists-component.tsx
+++ src/app/todo-lists/todo-lists-component.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { ScrollView, StyleSheet, Text, TextStyle, View, ViewStyle } from 'react-native';
+import { ScrollView, StyleSheet, Text, TextStyle, TouchableOpacity, View, ViewStyle } from 'react-native';
 import { TodoList } from '../todo-list/todo-list';
 
 export interface Props { }
@@ -33,30 +33,41 @@ export default class TodoListsComponent extends Component<Props, State> {
 
   private renderTodoLists() {
     const { todoLists } = this.state;
-    const { container, text } = styles;
+    const { container, touchable, text } = styles;
 
     return todoLists.map(todoList =>
       <View key={todoList.id}
             style={container}>
-        <Text style={text}>
-          {todoList.name}
-        </Text>
+        <TouchableOpacity style={touchable} onPress={() => this.navigateToList(todoList.id)}>
+          <Text style={text}>
+            {todoList.name}
+          </Text>
+        </TouchableOpacity>
       </View>
     );
+  }
+
+  private navigateToList(id: number) {
+    console.log(id);
   }
 }
 
 const styles: any = StyleSheet.create({
   text: {
-    fontSize: 15
+    fontSize: 15,
   } as TextStyle,
 
   container: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: 45,
     borderBottomWidth: 1,
     borderBottomColor: '#000',
+    height: 45,
     zIndex: 1
+  } as ViewStyle,
+
+  touchable: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'stretch',
+    flex: 1
   } as ViewStyle
 });
